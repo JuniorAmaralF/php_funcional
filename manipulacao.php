dae4d55654f03cfa3f6c8cdb08685288cfc5a125
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2022-04-25 20:48:55 -0300

    contando medalhas

--- manipulacao.php
+++ manipulacao.php
@@ -7,6 +7,19 @@ $contador = 0;
 
 echo "Número de paises: $contador\n"; 
 
+$brasil = $dados[0];
+
+function somaMedalhas(int $medalhasAcumuladas, int $medalhas){
+    return $medalhasAcumuladas + $medalhas;
+}
+
+$numeroDeMedalhas = array_reduce($brasil['medalhas'],'somaMedalhas',0);
+
+echo $numeroDeMedalhas;
+
+//exit();
+
+
 function convertePaisParaLetraMaiscula(array $pais): array {
     $pais['pais'] = mb_convert_case($pais['pais'],MB_CASE_UPPER);
     return $pais;
@@ -17,10 +30,19 @@ function verificaSePaisTemEspacoNoNome(array $pais):bool
     return strpos($pais['pais'], ' ') != false;
 }
 
+function medalhasAcumuladas(int $medalhasAcumuladas, array $pais){
+    return $medalhasAcumuladas + array_reduce($pais['medalhas'],'somaMedalhas',0);
+}
+
 
 $dados = array_map('convertePaisParaLetraMaiscula',$dados);
 $dados = array_filter($dados,'verificaSePaisTemEspacoNoNome');
 
 var_dump($dados);
+
+echo array_reduce($dados,'medalhasAcumuladas',0);
+
+
+
 
 ?>

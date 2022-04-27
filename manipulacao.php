1f3c56c21e84dd5755f055ba91c776be16b3d9ef
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2022-04-26 23:11:41 -0300

    simplifucando o codigo

--- manipulacao.php
+++ manipulacao.php
@@ -9,11 +9,14 @@ echo "Número de paises: $contador\n";
 
 $brasil = $dados[0];
 
-function somaMedalhas(int $medalhasAcumuladas, int $medalhas){
+$somaMedalhas = fn(int $medalhasAcumuladas, int $medalhas) => $medalhasAcumuladas + $medalhas;
+
+function somaMedalhas(int $medalhasAcumuladas, int $medalhas)
+{
     return $medalhasAcumuladas + $medalhas;
 }
 
-$numeroDeMedalhas = array_reduce($brasil['medalhas'],'somaMedalhas',0);
+$numeroDeMedalhas = array_reduce($brasil['medalhas'],$somaMedalhas,0);
 
 echo $numeroDeMedalhas;
 
@@ -25,40 +28,35 @@ function convertePaisParaLetraMaiscula(array $pais): array {
     return $pais;
 }
 
-function verificaSePaisTemEspacoNoNome(array $pais):bool
-{
-    return strpos($pais['pais'], ' ') != false;
-}
+$verificaSePaisTemEspacoNome = fn (array $pais):bool => strpos($pais['pais'], ' ') != false;
 
-function medalhasAcumuladas(int $medalhasAcumuladas, array $pais){
-    return $medalhasAcumuladas + array_reduce($pais['medalhas'],'somaMedalhas',0);
-}
+$medalhasAcumuladas = fn (int $medalhasAcumuladas, array $pais) => $medalhasAcumuladas + array_reduce($pais['medalhas'],'somaMedalhas',0);
 
 function pegaMedalhas(array $medalhas){
     return array_reduce($medalhas,'somaMedalhas',0);
 }
 
 
-function comparaMedalhas(array $medalhasPais1, array $medalhasPais2):callable
-{
-    return function(string $modalidade) use ($medalhasPais1,$medalhasPais2): int {
-        return $medalhasPais2[$modalidade] <=> $medalhasPais1[$modalidade];
-    };
-} 
+$comparaMedalhas = fn (array $medalhasPais1, array $medalhasPais2): callable
+    => fn (string $modalidade): int => $medalhasPais2[$modalidade] <=> $medalhasPais1[$modalidade];
+ 
 
 $dados = array_map('convertePaisParaLetraMaiscula',$dados);
-//$dados = array_filter($dados,'verificaSePaisTemEspacoNoNome');
+//$dados = array_filter($dados,$verificaSePaisTemEspacoNome);
 
 //var_dump($dados);
 
-echo array_reduce($dados,'medalhasAcumuladas',0);
+echo array_reduce($dados,$medalhasAcumuladas,0);
 
-
-$medalhas = array_reduce(array_map(function (array $medalhas){
-    return array_reduce($medalhas,'somaMedalhas',0);
-},array_column($dados,'medalhas')),
-'somaMedalhas',
-0
+//Arrow Functions  fn e o => sao uma redução na linha de codigo , partir 7.4
+//fn serio function . => seria o  {return codigo..}
+$medalhas = array_reduce(
+    array_map(
+        fn (array $medalhas) => array_reduce($medalhas,$somaMedalhas,0),
+        array_column($dados,'medalhas')
+    ),
+    $somaMedalhas,
+    0
 );
 
 $pais_medalhas = array_column($dados,'medalhas');
@@ -67,10 +65,10 @@ $soma_dos_tipo_medalha = array_map('pegaMedalhas',$pais_medalhas);
 
 $medalhas = array_reduce($soma_dos_tipo_medalha,'somaMedalhas',0);
 
-usort($dados, function(array $pais1, array $pais2){
+usort($dados, function(array $pais1, array $pais2) use ($comparaMedalhas){
     $medalhasPais1 = $pais1['medalhas'];
     $medalhasPais2 = $pais2['medalhas'];
-    $comparador = comparaMedalhas($medalhasPais1,$medalhasPais2);
+    $comparador = $comparaMedalhas($medalhasPais1,$medalhasPais2);
 
     $resultado = ($comparador('ouro') !== 0) ? $comparador('ouro')
         : ($comparador('prata') !== 0 ? $comparador('prata') 
@@ -81,7 +79,7 @@ usort($dados, function(array $pais1, array $pais2){
 
 var_dump($dados);
 
-
 echo $medalhas;
 
+    
 ?>
